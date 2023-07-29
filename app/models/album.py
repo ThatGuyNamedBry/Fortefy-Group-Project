9bebbc477502f1c7367ae982096f9b6a8be92da7
BryantS <bryantstine@yahoo.com>
2023-07-29 14:20:39 -0700

    working on deployment

--- app/models/album.py
+++ app/models/album.py
@@ -5,8 +5,8 @@ from datetime import datetime
 class Album(db.Model):
     __tablename__ = 'albums'
 
-    # if environment == "production":
-    #     __table_args__ = {'schema': SCHEMA}
+    if environment == "production":
+        __table_args__ = {'schema': SCHEMA}
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255), nullable=False, unique=True)
