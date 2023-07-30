de297e167ec1c0c539d6344fa951ab634b8be980
BryantS <bryantstine@yahoo.com>
2023-07-30 01:30:45 -0700

    finished albums crud

--- react-app/src/store/albums.js
+++ react-app/src/store/albums.js
@@ -1,15 +1,15 @@
 //                                           Action Types
-const LOAD_ALBUMS = 'spots/LOAD_ALBUMS';
-const LOAD_ALBUM = 'spots/LOAD_ALBUM';
-const CREATE_ALBUM = 'spots/CREATE_ALBUM';
-const UPDATE_ALBUM = 'spots/UPDATE_ALBUM';
-const DELETE_ALBUM = 'spots/DELETE_ALBUM';
+const LOAD_ALBUMS = 'albums/LOAD_ALBUMS';
+const LOAD_ALBUM = 'albums/LOAD_ALBUM';
+const CREATE_ALBUM = 'albums/CREATE_ALBUM';
+const UPDATE_ALBUM = 'albums/UPDATE_ALBUM';
+const DELETE_ALBUM = 'albums/DELETE_ALBUM';
 
 
 //                                         Action Creators
 
 //Get All Album Action
-export const getAllAlbumAction = (albums) => {
+export const getAllAlbumsAction = (albums) => {
   return {
     type: LOAD_ALBUMS,
     payload: albums,
@@ -50,10 +50,104 @@ export const deleteAlbumAction = (albumId) => {
 };
 
 //                                             Thunks
+//Get All Albums Thunk
+export const getAllAlbumsThunk = () => async (dispatch) => {
+  const response = await fetch('/api/albums');
+  const albums = await response.json();
+  dispatch(getAllAlbumsAction(albums.Albums));
+  return response;
+};
 
+//Get All Albums by Current User Thunk
+export const getCurrentUserAllAlbumsThunk = () => async (dispatch) => {
+  const response = await fetch('/api/albums/current');
+  const albums = await response.json();
+  dispatch(getAllAlbumsAction(albums.Albums));
+  return response;
+};
 
+//Get Album by ID Thunk
+export const getAlbumByIdThunk = (albumId) => async (dispatch) => {
+  const response = await fetch(`/api/albums/${albumId}`);
+  const album = await response.json();
+  dispatch(getAlbumByIdAction(album));
+  return response;
+};
 
+//Create an Album Thunk
+export const createAlbumThunk = (formData) => async (dispatch) => {
+  // console.log('Create album thunk running, this is the formData', formData)
+  const response = await fetch('/api/albums', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(formData),
+  });
+  // console.log('After fetch, this is the response', response)
+  if (response.ok) {
+    const album = await response.json();
+    // console.log('If response is okay running, this is album', album)
+    for (const image of formData.images) {
+      if (image.url) {
+        await dispatch(createImageforAlbumThunk(album, image));
+      }
+    }
+    return album;
+  } else {
+    const errorData = await response.json();
+    return errorData;
+  }
+};
 
+//Create Album Image Thunk
+export const createImageforAlbumThunk = (album, images) => async (dispatch) => {
+  // console.log('Create Image for Album Thunk, this is album and image ', album, images)
+  const response = await fetch(`/api/albums/${album.id}/images`, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(images),
+  });
+  if (response.ok) {
+    const newImage = await response.json();
+    // console.log('THIS IS NEW IMAGE RESPONSE', newImage)
+    album.url = newImage.url;
+    dispatch(createAlbumAction(album));
+    return newImage;
+  } else {
+    const errorData = await response.json();
+    return errorData;
+  }
+};
+
+//Edit/Update an Album Thunk
+export const updateAlbumThunk = (album) => async (dispatch) => {
+  // console.log('Edit/Update an album Thunk, this is album  ', album);
+  const response = await fetch(`/api/albums/${album.id}`, {
+    method: 'PUT',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(album),
+  });
+
+  if (response.ok) {
+    const album = await response.json();
+    dispatch(updateAlbumAction(album));
+    return album;
+  } else {
+    const errorData = await response.json();
+    return errorData;
+  }
+};
+
+//Delete an Album Thunk
+export const deleteAlbumThunk = (albumId) => async (dispatch) => {
+  const response = await fetch(`/api/albums/${albumId}`, {
+    method: 'DELETE',
+  });
+
+  if (response.ok) {
+    dispatch(deleteAlbumAction(albumId));
+    return response;
+  }
+};
 
 
 //Reducer function
--- react-app/src/store/index.js
+++ react-app/src/store/index.js
@@ -1,9 +1,11 @@
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import session from './session'
+import albumReducer from "./albums";
 
 const rootReducer = combineReducers({
   session,
+  albums: albumReducer,
 });
 
 
