581a5a7941b5e4a3fa38ddef8ad8d50a798abd45
BryantS <bryantstine@yahoo.com>
2023-07-30 16:20:36 -0700

    created song reducer

--- react-app/src/store/index.js
+++ react-app/src/store/index.js
@@ -2,10 +2,12 @@ import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import session from './session'
 import albumReducer from "./albums";
+import songReducer from "./songs";
 
 const rootReducer = combineReducers({
   session,
   albums: albumReducer,
+  songs: songReducer,
 });
 
 
