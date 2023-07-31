1af6b49e8c5366e5f3e8f46b57fd8ab0cdf11783
Alex <alex.j.basso@gmail.com>
2023-07-31 14:07:34 -0500

    Changed seed data to pull from AWS

--- app/seeds/albums.py
+++ app/seeds/albums.py
@@ -2,58 +2,50 @@ from app.models import db, environment, SCHEMA, Album
 from sqlalchemy.sql import text
 
 def seed_albums():
-    album1 = Album(
-        name='Album 1',
-        user_id=1,
-        art='https://upload.wikimedia.org/wikipedia/commons/e/ed/Compact_Disc.jpg',
-        artist='Artist 1',
-        year=2023,
-        genre='Rock'
-    )
 
-    album2 = Album(
-        name='Dark Side of the Moon',
+    album1 = Album(
+        name='The Dark Side of the Moon',
         user_id=1,
-        art='https://upload.wikimedia.org/wikipedia/en/3/3b/Dark_Side_of_the_Moon.png',
+        art='https://fortefy-song-url.s3.us-east-2.amazonaws.com/Pink+Floyd/The+Dark+Side+of+the+Moon/The+Dark+Side+of+the+Moon.jpg',
         artist='Pink Floyd',
         year=1973,
-        genre='Rock'
+        genre='Psychedelic Rock'
     )
-    album3 = Album(
+    album2 = Album(
         name='Stankonia',
         user_id=1,
-        art='https://upload.wikimedia.org/wikipedia/en/0/0b/OutKast_-_Stankonia.JPG',
+        art='https://fortefy-song-url.s3.us-east-2.amazonaws.com/OutKast/Stankonia/Stankonia.jpg',
         artist='OutKast',
         year=2000,
-        genre='Rap'
+        genre='Southern Hip Hop'
     )
-    album4 = Album(
+    album3 = Album(
         name='Section.80',
         user_id=2,
-        art='https://i.redd.it/uko5illmwhx01.jpg',
+        art='https://fortefy-song-url.s3.us-east-2.amazonaws.com/Kendrick+Lamar/Section.80/Section.80.jpg',
         artist='Kendrick Lamar',
         year=2011,
-        genre='Rap'
+        genre='West Coast Hip Hop'
     )
-    album5 = Album(
-        name="Queen's Greatest Hits",
+    album4 = Album(
+        name="Greatest Hits",
         user_id=2,
-        art='https://upload.wikimedia.org/wikipedia/en/0/02/Queen_Greatest_Hits.png',
+        art='https://fortefy-song-url.s3.us-east-2.amazonaws.com/Queen/Greatest+Hits/Greatest+Hits.jpg',
         artist='Queen',
         year=1981,
-        genre='Rock'
+        genre='Glam Rock'
     )
-    album6 = Album(
-        name="Blink 182's Greatest Hits",
+    album5 = Album(
+        name="Greatest Hits",
         user_id=3,
-        art='https://m.media-amazon.com/images/M/MV5BMTMwMjI3NjU4OV5BMl5BanBnXkFtZTcwNzQxNjM0Mg@@._V1_FMjpg_UX1000_.jpg',
-        artist='Blink 182',
+        art='https://fortefy-song-url.s3.us-east-2.amazonaws.com/blink-182/Greatest+Hits/Greatest+Hits.jpg',
+        artist='blink-182',
         year=2005,
-        genre='Rock'
+        genre='Pop Punk'
     )
 
 
-    db.session.add_all([album1, album2, album3, album4, album5, album6])
+    db.session.add_all([album1, album2, album3, album4, album5])
     db.session.commit()
 
 def undo_albums():
--- app/seeds/songs.py
+++ app/seeds/songs.py
@@ -2,124 +2,107 @@ from app.models import db, User, environment, SCHEMA, Song
 from sqlalchemy.sql import text
 
 def seed_songs():
+
     song1 = Song(
-        name='Song 1',
-        user_id=1,
-        album_id=1,
-        duration=184,
-        song_url='fakesongurl1',
-        track_number=1
-    )
-
-    song2 = Song(
-        name="Song 2",
-        user_id=1,
-        album_id=1,
-        duration=111,
-        song_url='fakesongurl2',
-        track_number=2
-    )
-
-    song3 = Song(
         name="Speak to Me",
         user_id=1,
         album_id=2,
         duration=65,
-        song_url='fakesongurl2',
+        song_url='https://fortefy-song-url.s3.us-east-2.amazonaws.com/Pink+Floyd/The+Dark+Side+of+the+Moon/Speak+to+Me.mp3',
         track_number=1
     )
 
-    song4 = Song(
+    song2 = Song(
         name="Breathe (In the Air)",
         user_id=1,
         album_id=2,
         duration=169,
-        song_url='fakesongurl2',
+        song_url='https://fortefy-song-url.s3.us-east-2.amazonaws.com/Pink+Floyd/The+Dark+Side+of+the+Moon/Breathe+(In+the+Air).mp3',
         track_number=2
     )
 
-    song5 = Song(
+    song3 = Song(
         name="So Fresh So Clean",
         user_id=1,
         album_id=3,
         duration=240,
-        song_url='fakesongurl2',
-        track_number=1
+        song_url='https://fortefy-song-url.s3.us-east-2.amazonaws.com/OutKast/Stankonia/So+Fresh%2C+So+Clean.mp3',
+        track_number=4
     )
 
-    song6 = Song(
+    song4 = Song(
         name="Ms. Jackson",
         user_id=1,
         album_id=3,
         duration=270,
-        song_url='fakesongurl2',
-        track_number=2
+        song_url='https://fortefy-song-url.s3.us-east-2.amazonaws.com/OutKast/Stankonia/Ms.+Jackson.mp3',
+        track_number=5
     )
 
-    song7 = Song(
-        name="Ronald Reagan Era",
+    song5 = Song(
+        name="Ronald Reagan Era (His Evils)",
         user_id=2,
         album_id=4,
         duration=216,
-        song_url='fakesongurl2',
-        track_number=1
+        song_url='https://fortefy-song-url.s3.us-east-2.amazonaws.com/Kendrick+Lamar/Section.80/Ronald+Reagan+Era+(His+Evils).mp3',
+        track_number=7
     )
 
-    song8 = Song(
-        name="HiiiPower",
+    song6 = Song(
+        name="HiiiPoWeR",
         user_id=2,
         album_id=4,
         duration=279,
-        song_url='fakesongurl2',
-        track_number=2
+        song_url='https://fortefy-song-url.s3.us-east-2.amazonaws.com/Kendrick+Lamar/Section.80/HiiiPoWeR.mp3',
+        track_number=16
     )
 
-    song9 = Song(
-        name="Rigamortus",
+    song7 = Song(
+        name="Rigamortis",
         user_id=2,
         album_id=4,
         duration=168,
-        song_url='fakesongurl2',
-        track_number=3
+        song_url='https://fortefy-song-url.s3.us-east-2.amazonaws.com/Kendrick+Lamar/Section.80/Rigamortis.mp3',
+        track_number=12
     )
 
-    song10 = Song(
+    song8 = Song(
         name="Bohemian Rhapsody",
         user_id=2,
         album_id=5,
         duration=355,
-        song_url='fakesongurl2',
+        song_url='https://fortefy-song-url.s3.us-east-2.amazonaws.com/Queen/Greatest+Hits/Bohemian+Rhapsody.mp3',
         track_number=1
     )
 
-    song11 = Song(
+    song9 = Song(
         name="We Will Rock You",
         user_id=2,
         album_id=5,
         duration=121,
-        song_url='fakesongurl2',
-        track_number=2
+        song_url='https://fortefy-song-url.s3.us-east-2.amazonaws.com/Queen/Greatest+Hits/We+Will+Rock+You.mp3',
+        track_number=16
     )
 
-    song12 = Song(
-        name="All of the Small Things",
+    song10 = Song(
+        name="All the Small Things",
         user_id=3,
         album_id=6,
         duration=171,
-        song_url='fakesongurl2',
-        track_number=1
+        song_url='https://fortefy-song-url.s3.us-east-2.amazonaws.com/blink-182/Greatest+Hits/All+the+Small+Things.mp3',
+        track_number=6
     )
 
-    song13 = Song(
+    song11 = Song(
         name="I Miss You",
         user_id=3,
         album_id=6,
         duration=227,
-        song_url='fakesongurl2',
-        track_number=2
+        song_url='https://fortefy-song-url.s3.us-east-2.amazonaws.com/blink-182/Greatest+Hits/I+Miss+You.mp3',
+        track_number=13
     )
 
-    db.session.add_all([song1, song2, song3, song4, song5, song6, song7, song8, song9, song10, song11, song12, song13])
+    db.session.add_all([song1, song2, song3, song4, song5, song6, song7, song8, song9, song10, song11])
     db.session.commit()
 
 def undo_songs():
