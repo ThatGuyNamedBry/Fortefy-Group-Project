484a8bea519dd2d110a5dbb8ec12869f6ce2b9a4
Angad Bhatia <128391290+Angad-Bhatia@users.noreply.github.com>
2023-07-31 19:09:32 -0400

    progress on like

--- react-app/src/components/LikeButton/index.js
+++ react-app/src/components/LikeButton/index.js
@@ -1,22 +1,28 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { addLikeThunk, removeLikeThunk } from '../../store/songs';
 import './LikeButton.css'
 
 const LikeButton = ({ songId }) => {
     const dispatch = useDispatch();
-    const song = useSelector(state => state.songs.singleSong['songId']);
+    const song = useSelector(state => state.songs.allSongs['songId']);
     const user = useSelector(state => state.session.user);
+    const [userLike, setUserLike] = useState(false)
     const [isLiked, setIsLiked] = useState('');
     useEffect(() => {
-        const likes = song.likes;
-        if (user) {
-            const userLike = likes.find(like => like.user_id === user.id);
+        setIsLiked('');
+        if (user && song) {
+            setUserLike(song.likes.find(like => like.user_id === user.id));
             setIsLiked(userLike ? 'fill' : '');
         }
     }, [song, user])
     const likeClick = (e) => {
-        e.stopPropogation();
-
+        e.stopPropagation();
+        if (!isLiked) {
+            dispatch(addLikeThunk(songId));
+        } else {
+            dispatch(removeLikeThunk(songId, userLike.id));
+        }
     };
 
     if (!user || (song && song.user_id === user.id)) {
