4e216221e8f934ededa9d60297fef1de0423a002
Angad Bhatia <128391290+Angad-Bhatia@users.noreply.github.com>
2023-08-02 03:46:22 -0400

    all basic playlist backend crud complete

--- app/api/playlist_routes.py
+++ app/api/playlist_routes.py
@@ -1,9 +1,13 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 from flask_login import login_required, current_user
 from app.models import db, Playlist
+from app.forms import CreatePlaylistForm, EditPlaylistForm
+from app.api.auth_routes import validation_errors_to_error_messages
+from app.api.aws_helper import get_unique_filename, upload_file_to_s3
 
 playlist_routes = Blueprint('playlists', __name__)
 
+# Get All Playlists
 @playlist_routes.route('')
 def get_all_playlists():
     """
@@ -12,6 +16,7 @@ def get_all_playlists():
     playlists = [playlist.to_dict() for playlist in Playlist.query.all()]
     return jsonify(playlists)
 
+# Get a Playlist By Id
 @playlist_routes.route('/<int:id>')
 def get_playlist_by_id(id):
     """
@@ -20,6 +25,7 @@ def get_playlist_by_id(id):
     playlist = Playlist.query.get(id)
     return jsonify(playlist.to_dict())
 
+# Get All User-Created Playlists
 @playlist_routes.route('/current')
 @login_required
 def get_user_playlists():
@@ -30,17 +36,54 @@ def get_user_playlists():
     playlists_dict = [playlist.to_dict() for playlist in user_playlists]
     return jsonify(playlists_dict)
 
+# Create a New Playlist
 @playlist_routes.route('/new', methods=['POST'])
 @login_required
 def create_new_playlist():
-    return
+    form = CreatePlaylistForm()
+    form['csrf_token'].data = request.cookies['csrf_token']
 
-@playlist_routes.route('/<int:id>/edit')
+    if form.validate_on_submit():
+        new_playlist = Playlist(
+            user_id = current_user.id,
+            title = form.data['title'],
+            art = form.data['art'],
+            description = form.data['description']
+        )
+
+        db.session.add(new_playlist)
+        db.session.commit()
+        return jsonify(new_playlist.to_dict())
+
+    # print(validation_errors_to_error_messages(form.errors))
+    return { 'errors': validation_errors_to_error_messages(form.errors)}, 400
+
+# Edit a User-Created Playlist
+@playlist_routes.route('/<int:id>/edit', methods=['PUT'])
 @login_required
 def edit_playlist(id):
-    return
+    form = EditPlaylistForm()
+    form['csrf_token'].data = request.cookies['csrf_token']
 
-@playlist_routes.route('/<int:id>/delete')
+    if form.validate_on_submit():
+        playlist = Playlist.query.get(id)
+
+        if playlist is None:
+            return { 'errors': 'Playlist not found'}, 404
+        elif playlist.user_id != current_user.id:
+            return { 'errors': 'Playlist does not belong to user' }, 403
+
+        playlist.title = form.data['title']
+        playlist.art = form.data['art']
+        playlist.description = form.data['description']
+
+        db.session.commit()
+
+        return jsonify(playlist.to_dict())
+    return { 'errors': validation_errors_to_error_messages(form.errors) }, 400
+
+# Delete a Playlist By Id
+@playlist_routes.route('/<int:id>/delete', methods=['DELETE'])
 @login_required
 def delete_playlist(id):
     playlist = Playlist.query.get(id)
