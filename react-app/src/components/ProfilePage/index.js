b3f126cabc6fec1c35eb45fed77b6ec65380d586
Joshua <jhoang304@gmail.com>
2023-08-03 12:29:01 -0500

    added create album button and moved tile buttons inside containers

--- react-app/src/components/ProfilePage/index.js
+++ react-app/src/components/ProfilePage/index.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { useHistory, Link } from 'react-router-dom';
+import { useHistory, Link, NavLink } from 'react-router-dom';
 import { getCurrentUserAllAlbumsThunk } from '../../store/albums';
 import { getCurrentUserAllSongsThunk } from '../../store/songs';
 import { getCurrentUserAllPlaylistsThunk } from '../../store/playlists';
@@ -36,15 +36,21 @@ const ProfilePage = () => {
   return (
     <div className="profile-container">
       <h1>{user?.name}</h1>
-
       <div className="section-container">
+        <div className="album-header">
         <h2>Your Albums</h2>
+        {user && (
+				<NavLink to="/albums/new" className="create-album-button">
+					<i class="fa-solid fa-circle-plus"></i>
+				</NavLink>
+			  )}
+        </div>
         <div className="album-grid">
           {userAlbums.map((album) => (
             <div key={album?.id} className="profile-tile-container">
               <div className="profile-tile-buttons">
                 <div onClick={() => handleUpdateAlbum(album)} className='update-delte-music-buttons fa-solid fa-pen-to-square'></div>
-                <DeleteMusicButton modalComponent={<DeleteModal className="delete-song-modal" type='album' id={album.id} />} />
+                <DeleteMusicButton className="delete-song-modal" modalComponent={<DeleteModal type='album' id={album.id} />} />
               </div>
               <Link to={`/albums/${album.id}`} className="album-tile link-as-text">
                 <img src={album.art} alt={album.name} className="album-image" />
