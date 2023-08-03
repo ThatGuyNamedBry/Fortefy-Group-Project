549fe80d0c313ab19433938d571b7543af83efb8
Angad Bhatia <128391290+Angad-Bhatia@users.noreply.github.com>
2023-08-03 19:46:01 -0400

    started add playlist song functionality

--- react-app/src/components/AddPLSongButton/index.js
+++ react-app/src/components/AddPLSongButton/index.js
@@ -1,18 +1,31 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { getCurrentUserAllPlaylistsThunk } from '../../store/playlists';
+import { getCurrentUserAllPlaylistsThunk, addPlaylistSongThunk } from '../../store/playlists';
 import './AddPLSong';
 
 const AddPLSongButton = ({ songId }) => {
     const dispatch = useDispatch();
+
     const user = useSelector(state => state.session.user);
     const playlistsObject = useSelector(state => state.playlists.allPlaylists);
     const playlists = Object.values(playlistsObject);
+
+    const [showPlaylists, setShowPlaylists] = useState(false);
+
+
+
     useEffect(() => {
         dispatch(getCurrentUserAllPlaylistsThunk());
     }, [dispatch]);
+
+    const selectPlaylistClick = (e, playlistId) => {
+        e.stopPropagation();
+        dispatch(addPlaylistSongThunk(playlistId, songId));
+    }
     return (
-        <div></div>
+        <button>
+
+        </button>
     )
 };
 
